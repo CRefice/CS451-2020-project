91cd80095ccd84d711f9dc76a134c9f7cf298c42
Carlo Refice <CRefice@gmail.com>
2020-11-13 12:37:50 +0100

    Add bin folders to submission zip

--- submit.sh
+++ submit.sh
@@ -7,7 +7,7 @@ SUBMISSION_DIR="$TEMPDIR/$SCIPER"
 mkdir -p "$SUBMISSION_DIR"
 
 ./code/cleanup.sh
-cp -r code/CMakeLists.txt code/build.sh code/cleanup.sh code/src/ "$SUBMISSION_DIR"
+cp -r code/bin code/CMakeLists.txt code/build.sh code/run.sh code/.gitignore code/cleanup.sh code/src/ "$SUBMISSION_DIR"
 (cd $TEMPDIR; zip -r ${SCIPER}.zip $SCIPER)
 mv "$TEMPDIR/${SCIPER}.zip" .
 rm -r "$TEMPDIR"
