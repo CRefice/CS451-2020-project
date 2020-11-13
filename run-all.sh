16117e79924065b9bab6ed8d39ca972e30820ce0
Carlo Refice <CRefice@gmail.com>
2020-11-13 12:23:14 +0100

    Update num_processes in run script

--- run-all.sh
+++ run-all.sh
@@ -2,7 +2,7 @@ mkdir -p logs
 
 trap "kill 0" EXIT
 
-NUM_PROCESSES=3
+NUM_PROCESSES=4
 python finishedSignal.py --host localhost --port 10000 -p $NUM_PROCESSES &
 python barrier.py --host localhost --port 11000 -p $NUM_PROCESSES &
 
