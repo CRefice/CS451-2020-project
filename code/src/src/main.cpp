5bcee78e9d95cdb48b2775849bfd990067aa5666
Carlo Refice <CRefice@gmail.com>
2020-12-11 16:53:46 +0100

    Final final touches

--- code/src/src/main.cpp
+++ code/src/src/main.cpp
@@ -103,11 +103,7 @@ int main(int argc, char** argv) {
   std::cout << "Signaling end of broadcasting messages\n\n";
   coordinator.finishedBroadcasting();
 
-  auto to_receive = num_messages * hosts.size();
-  while (log.received_count() < to_receive) {
-    link.receive(message_queue.pop());
-  }
-
   while (true) {
+    link.receive(message_queue.pop());
   }
 }
